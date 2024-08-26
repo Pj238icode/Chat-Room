1b93ee4b18a8ea9f5d595d80013ce6a524474513
Pj238icode <122697153+Pj238icode@users.noreply.github.com>
2024-08-26 16:38:25 +0530

    Chat Room

---
+++ Partials/Dashnav.php
@@ -0,0 +1,5 @@
+<aside class="sidebar">
+                <h5>Active Users</h5>
+                <ul class="user-list" id="user_list">
+
+                </ul>
---
+++ Partials/navbar.php
@@ -0,0 +1,3 @@
+<nav class="navbar navbar-light bg-light">
+    <span class="navbar-brand mb-0 h1 text-primary fw-bold">CHAT ROOM</span>
+</nav>
